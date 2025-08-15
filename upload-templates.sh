fe9c16ac062ebe741934182cb01e48f25aeba6c4
Sharada Weerasinghe <sharadashehan6@gmail.com>
2025-08-15 23:53:33 +0530

    feat: Add SES email storage parameters and resources to CloudFormation templates

--- upload-templates.sh
+++ upload-templates.sh
@@ -49,6 +49,7 @@ upload_templates() {
     aws s3 cp infrastructure/frontend-website.yaml s3://$CLOUDFORMATION_BUCKET/frontend-website.yaml --region $AWS_REGION
     aws s3 cp infrastructure/backup-system.yaml s3://$CLOUDFORMATION_BUCKET/backup-system.yaml --region $AWS_REGION
     aws s3 cp infrastructure/ses-bounce-complaint-system.yaml s3://$CLOUDFORMATION_BUCKET/ses-bounce-complaint-system.yaml --region $AWS_REGION
+    aws s3 cp infrastructure/ses-email-storage.yaml s3://$CLOUDFORMATION_BUCKET/ses-email-storage.yaml --region $AWS_REGION
     
     print_success "All CloudFormation templates uploaded successfully!"
 }
